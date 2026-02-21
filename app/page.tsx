7f13e880b63730b837204652c8710baa2b7db703
Cole Sladowsky <coleslad31@gmail.com>
2026-02-21 17:36:09 -0500

    fix wording

--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
             </div>
             <div className="exp-company">MITRE Corporation</div>
             <ul className="exp-bullets">
-              <li className="coming-soon">details coming soon</li>
+              <li className="coming-soon">coming soon</li>
             </ul>
           </div>
 
