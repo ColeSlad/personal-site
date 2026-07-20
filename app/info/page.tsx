aaef4121313b37b6ea2dde03c17980db4877a371
Cole Sladowsky <coleslad31@gmail.com>
2026-07-19 17:32:38 -0700

    make background black across all pages

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -24,7 +24,7 @@ export default function Info() {
           <p className="info-body">
             i love to learn by doing, and i&apos;m always working on projects
             to explore new ideas and sharpen my skills. when i&apos;m not
-            coding, you can find me playing at the gym, watching basketball,
+            coding, you can find me at the gym, watching basketball,
             or spending time with friends and family. feel free to reach me at{' '}
             <a href="mailto:coleslad31@gmail.com">coleslad31@gmail.com</a>.
           </p>
